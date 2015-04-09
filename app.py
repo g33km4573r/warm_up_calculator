f0049f7c29997fc455da60efdb1b6fe37eb4fc4d
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-09 15:42:05 -0400

    add form

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
 from flask import Flask, render_template
+from flask.ext.wtf import Form
+from flask_wtf.html5 import NumberInput
+from wtforms import StringField
+from wtforms.validators import Required
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = os.environ.get("SECRET_KEY")
+
+class PlatesForm(Form):
+    plates = StringField("plates", validators=[Required()])
+    goal = StringField("goal", validators=[Required()])
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html") 
+    return render_template("index.html", form=PlatesForm()) 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- tests/test_live_website.py
+++ tests/test_live_website.py
@@ -1,4 +1,22 @@
 import pytest
 
 
+HOME_URL = "https://radiant-stream-4011.herokuapp.com/"
 
+@pytest.mark.webtest
+def  test_warm_up_calculator(browser):
+
+    browser.visit(HOME_URL)
+    title = browser.find_by_tag("title").first
+    assert title.html == "Warm Up Calculator"
+
+    # submit data 
+    browser.fill_form({"plates": "20x5", "goal": "100"})
+    browser.find_by_name("submit").first.click()
+
+    # view results
+    # second_goal = browser.find_by_id("warm-up-goal-2").first()
+    # assert second_goal.html == "40"
+
+    # second_warm_up_set = browser.find_by_id("warm-up-set-2").first()
+    # assert second_warm_up_set.html == "20, 20"
