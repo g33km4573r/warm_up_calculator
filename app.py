fbc0acf0f3a2223b47e29a83ad21b610d6a7f2a2
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-14 12:08:44 -0400

    add submit button

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from collections import Counter
 from flask import Flask, render_template, request
 from flask.ext.wtf import Form
 from flask_wtf.html5 import NumberInput
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.bootstrap import Bootstrap
 
@@ -17,6 +17,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 class PlatesForm(Form):
     plates = StringField("plates", validators=[Required()])
     goal = StringField("goal", validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 @app.route("/", methods=["GET", "POST"])
