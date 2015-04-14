b2fc8303d56a437b79d93cef787584752ca0b086
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-14 12:36:59 -0400

    test data

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ class PlatesForm(Form):
 def index():
     warm_up = None
     if request.method == "POST":
-        warm_up = which_plates(
-            float(request.form['goal']),
-            Counter([float(p) for p in request.form['plates'].split(',')]),
-            [.20, .40, .60, .80, 1]
-        )
-        warm_up = [(plates, sum(plates)) for action, plates in warm_up if action=="l"]
+        # warm_up = which_plates(
+        #     float(request.form['goal']),
+        #     Counter([float(p) for p in request.form['plates'].split(',')]),
+        #     [.20, .40, .60, .80, 1]
+        # )
+        # warm_up = [(plates, sum(plates)) for action, plates in warm_up if action=="l"]
+        warm_up = [('set'),('goal')]
 
     return render_template("index.html", form=PlatesForm(), warm_up=warm_up) 
 
