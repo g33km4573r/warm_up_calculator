4d434f1383aa27e26daf872d7c1b23980c3a19e5
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-29 13:33:33 -0400

    change req

--- tests/test_live_website.py
+++ tests/test_live_website.py
@@ -15,8 +15,8 @@ def  test_warm_up_calculator(browser):
     browser.find_by_name("submit").first.click()
 
     # view results
-    # second_goal = browser.find_by_id("warm-up-goal").first()
-    # assert second_goal.html == "40"
+    second_goal = browser.find_by_id("warm-up-goal").first()
+    assert second_goal.html == "40"
 
-    # second_warm_up_set = browser.find_by_id("warm-up-set-2").first()
-    # assert second_warm_up_set.html == "20, 20"
+    second_warm_up_set = browser.find_by_id("warm-up-set-2").first()
+    assert second_warm_up_set.html == "20, 20"
